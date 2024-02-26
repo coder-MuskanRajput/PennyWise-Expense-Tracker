1069b253a2c15c38c33e8cd598ef590cd4ab7680
coder-MuskanRajput <muskanrajput1510@gmail.com>
2024-02-26 23:16:24 +0530

    Atlas

--- models/config.js
+++ models/config.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/ExpenseTracker")
+mongoose.connect(process.env.MONGODB)
 .then(()=>console.log("Database Connected"))
 .catch((err) => console.log(err.message))               
